cccb14710e9bde686d2f747bd0264b9174e88371
Axel SAGREDO <axel.sagredo@hotmail.fr>
2023-10-31 23:38:39 +0100

    test2

--- pieces.js
+++ pieces.js
@@ -1,5 +1,6 @@
 const response = await fetch("pieces-auto.json")
 const pieces = await response.json()
+console.log("hey")
 
 const article1 = pieces[0]
 const imageElement = document.createElement("img")
@@ -10,9 +11,11 @@ const prixElement = document.createElement("p")
 prixElement.innerText = `Prix : ${article1.prix}€` 
 const categorieElement = document.createElement("p")
 categorieElement = article1.categorie 
+console.log("salut")
 
 const sectionFiches = document.querySelector(".fiches")
 sectionFiches.appendChild(imageElement)
 sectionFiches.appendChild(nomElement)
 sectionFiches.appendChild(prixElement)
 sectionFiches.appendChild(categorieElement)
+console.log("coucou")
